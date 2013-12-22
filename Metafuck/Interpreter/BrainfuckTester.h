c7d5f7406240da48aba24aa759a11c720abc59fa
5cript <tim06TR@gmail.com>
2013-12-22 02:47:30 +0100

    added TestResults container + class for entries
    added enums for:
    - severity level
    - error id
    
    added map for error_id -> message_string
    
    added memberfunctions for the different checking levels

--- Metafuck/Interpreter/BrainfuckTester.h
+++ Metafuck/Interpreter/BrainfuckTester.h
@@ -5,17 +5,56 @@
 
 #include <string>
 #include <sstream>
+#include <map>
 
 namespace BrainfuckCompiler {
 
+	enum BrainfuckTestLevel {
+		BFTL_SYNTAX,		// check compiler verify (can the code actually be compiled?) and do obvious checkings
+		BFTL_PREDICTION,	// enable error prediction
+		BFTL_THOROUGH		// run the program and log overflows and memptr offset failures (!DO NOT USE WITH PROGRAMS THAT DO NOT TERMINATE!)
+	};
+
+	enum ErrorMessageID {
+		EID_UNKNOWN_ERROR,
+		EID_INVALID_LOOPS,
+		EID_BAILING_OUT
+	};
+
+	enum EntrySeverityLevel {
+		ESL_INFORMATION,
+		ESL_WARNING,
+		ESL_ERROR,
+		ESL_CRITICAL
+	};
+
+	extern const std::map <ErrorMessageID, std::string> errors;
+
+	struct TestResultEntry {
+		EntrySeverityLevel serverity_level;
+		ErrorMessageID message_id;
+	};
+
+	class TestResults {
+	public:
+		void addEntry(EntrySeverityLevel severity_level, ErrorMessageID message_id);
+		bool containsEntryOfLevel(EntrySeverityLevel severity_level);
+	private:
+		std::vector <TestResultEntry> entries_;
+	};
+
 	class BrainfuckTester {
 	public:
-		BrainfuckTester(std::string code, std::string input = "");
+		BrainfuckTester();
+		void runTest(std::string code, std::string input, VirtualMemory& memory, BrainfuckTestLevel level = BFTL_SYNTAX);
 
 	private:
-		std::string input_;
-		std::stringstream output_;
-		BrainfuckProgram <std::string, std::stringstream> program_;
+		TestResults results_;
+
+	private: // member function
+		void runSyntaxCheck(std::string const& code);
+		void runPredictionEngine(std::string const& code);
+		void checkRuntime(std::string const& code);
 	};
 
 } // namespace BrainfuckCompiler
