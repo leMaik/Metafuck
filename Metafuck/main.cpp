92317cac0367c7aa130440450745f961275542e0
leMaik <maik@maik.tk>
2013-12-25 02:45:11 +0100

    Added some command line params (will remake that soon!)

--- Metafuck/main.cpp
+++ Metafuck/main.cpp
@@ -2,34 +2,53 @@
 #include "brainfuck.h"
 #include "Interpreter\ConvenienceFuck.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <stack>
 
-#define STACK_SIZE 42
-
-using namespace std;
-
 void pause() {
-	cin.clear();
-	cin.get();
+	std::cin.clear();
+	std::cin.get();
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-	string input;
-	getline(cin, input);
-	Compiler com(input);
+	std::string input;
+	if (argc >= 2){
+		std::ifstream in;
+		in.open(argv[1]);
+		std::stringstream strStream;
+		strStream << in.rdbuf();//read the file
+		input = strStream.str();
+		in.close();
+	}
+	else {
+		std::getline(std::cin, input);
+	}
+	bool shallRun = false;
 
-	cout << "Compiling...\n";
-	cout << com.lex() << " statements found.\n";
+	Compiler com(input);
+	com.lex();
 	com.compile();
 	//com.optimize();
-	cout << "===\n";
-	cout << com.getGeneratedCode() << "\n";
-	cout << "===\nRunning:\n";
-	RunBrainfuckProgram(com.getGeneratedCode());
-	cout << "\n===\n";
-	pause();
 
+	if (argc >= 3 && argv[2]) {
+		std::ofstream out;
+		out.open(argv[2]);
+		out << com.getGeneratedCode();
+		out.close();
+	}
+	else {
+		std::cout << com.getGeneratedCode() << std::endl;
+	}
+
+	if (shallRun) {
+		std::cout << "===" << std::endl << "Running:" << std::endl;
+		RunBrainfuckProgram(com.getGeneratedCode());
+		std::cout << std::endl << "===" << std::endl;;
+		pause();
+	}
+	std::cout << "Done. :)";
 	return 0;
 }
