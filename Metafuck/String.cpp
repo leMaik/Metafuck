eb11eb27acbd764190c59de9b787ec438fd23050
leMaik <maik@maik.tk>
2014-05-26 13:47:41 +0200

    Added escape character \r.

--- Metafuck/String.cpp
+++ Metafuck/String.cpp
@@ -1,9 +1,9 @@
 #include "String.h"
 #include "helper.h"
 
-
 String::String(std::string s) : value_(s)
 {
+	ReplaceStringInPlace(value_, "\\r", "\r");
 	ReplaceStringInPlace(value_, "\\n", "\n");
 	ReplaceStringInPlace(value_, "\\t", "\t");
 	ReplaceStringInPlace(value_, "\\\"", "\"");
